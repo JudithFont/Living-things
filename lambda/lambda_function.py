640694200d026da829cb4a180be0330be1083a7b
JudithFont <43608773+JudithFont@users.noreply.github.com>
2020-06-22 11:20:07 +0200

    Add files via upload

---
+++ lambda/lambda_function.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+"""Simple fact sample app."""
+
+import random
+import logging
+import json
+import prompts
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import (
+    AbstractRequestHandler, AbstractExceptionHandler,
+    AbstractRequestInterceptor, AbstractResponseInterceptor)
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model import Response
+
+sb = SkillBuilder()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+ANIMALS = [
+    'The heart of a shrimp is located in its head.',
+    'A snail can sleep for three years.',
+    'Slugs have four noses.',
+    'It is possible to hypnotize a frog by placing it on its back and gently stroking its stomach.',
+    'A cow gives nearly 200,000 glasses of milk in a lifetime.'
+]
+PLANTS = [
+    'Leaving the skin on potatoes while cooking is healthier as all the vitamins are in the skin.',
+    'Banana is an Arabic word for fingers.',
+    'Carrots were originally purple in colour.',
+    'The baobab tree found in Africa can store 1,000 to 120,000 litres of water in its swollen trunk.',
+    'Bamboo is the fastest-growing woody plant in the world; it can grow 35 inches in a single day.'
+]
+FACTS= ANIMALS + PLANTS
+
+# Built-in Intent Handlers
+class GetNewFactHandler(AbstractRequestHandler):
+    """Handler for Skill Launch and GetNewFact Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_request_type("LaunchRequest")(handler_input) or
+                is_intent_name("GetNewFactIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In GetNewFactHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+        
+        dades = handler_input.request_envelope.request
+        try:
+            lb_value = dades.intent.slots['living_beigns'].value
+        except AttributeError:
+            lb_value = None
+        print(lb_value)
+        
+        if lb_value is None:
+            random_fact = random.choice(FACTS)
+        elif "animal" in lb_value:
+            random_fact =random.choice(ANIMALS)
+        elif "plant" in lb_value:
+            random_fact = random.choice(PLANTS)
+        else:
+            random_fact = "If you hear this something has gone really wrong"
+        
+        speech = data[prompts.GET_FACT_MESSAGE].format(random_fact)
+
+        handler_input.response_builder.speak(speech).set_card(
+            SimpleCard(data[prompts.SKILL_NAME], random_fact))
+        return handler_input.response_builder.response
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In HelpIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.HELP_MESSAGE]
+        reprompt = data[prompts.HELP_REPROMPT]
+        handler_input.response_builder.speak(speech).ask(
+            reprompt).set_card(SimpleCard(
+                data[prompts.SKILL_NAME], speech))
+        return handler_input.response_builder.response
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In CancelOrStopIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.STOP_MESSAGE]
+        handler_input.response_builder.speak(speech)
+        return handler_input.response_builder.response
+
+
+class FallbackIntentHandler(AbstractRequestHandler):
+    """Handler for Fallback Intent.
+
+    AMAZON.FallbackIntent is only available in en-US locale.
+    This handler will not be triggered except in that locale,
+    so it is safe to deploy on any locale.
+    """
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In FallbackIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.FALLBACK_MESSAGE]
+        reprompt = data[prompts.FALLBACK_REPROMPT]
+        handler_input.response_builder.speak(speech).ask(
+            reprompt)
+        return handler_input.response_builder.response
+
+
+class LocalizationInterceptor(AbstractRequestInterceptor):
+    """
+    Add function to request attributes, that can load locale specific data.
+    """
+
+    def process(self, handler_input):
+        locale = handler_input.request_envelope.request.locale
+        logger.info("Locale is {}".format(locale))
+
+        # localized strings stored in language_strings.json
+        with open("language_strings.json") as language_prompts:
+            language_data = json.load(language_prompts)
+        # set default translation data to broader translation
+        if locale[:2] in language_data:
+            data = language_data[locale[:2]]
+            # if a more specialized translation exists, then select it instead
+            # example: "fr-CA" will pick "fr" translations first, but if "fr-CA" translation exists,
+            # then pick that instead
+            if locale in language_data:
+                data.update(language_data[locale])
+        else:
+            data = language_data[locale]
+        handler_input.attributes_manager.request_attributes["_"] = data
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In SessionEndedRequestHandler")
+
+        logger.info("Session ended reason: {}".format(
+            handler_input.request_envelope.request.reason))
+        return handler_input.response_builder.response
+
+
+# Exception Handler
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Catch all exception handler, log exception and
+    respond with custom message.
+    """
+
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.info("In CatchAllExceptionHandler")
+        logger.error(exception, exc_info=True)
+
+        handler_input.response_builder.speak(EXCEPTION_MESSAGE).ask(
+            HELP_REPROMPT)
+
+        return handler_input.response_builder.response
+
+
+# Request and Response loggers
+class RequestLogger(AbstractRequestInterceptor):
+    """Log the alexa requests."""
+
+    def process(self, handler_input):
+        # type: (HandlerInput) -> None
+        logger.debug("Alexa Request: {}".format(
+            handler_input.request_envelope.request))
+
+
+class ResponseLogger(AbstractResponseInterceptor):
+    """Log the alexa responses."""
+
+    def process(self, handler_input, response):
+        # type: (HandlerInput, Response) -> None
+        logger.debug("Alexa Response: {}".format(response))
+
+
+# Register intent handlers
+sb.add_request_handler(GetNewFactHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+
+# Register exception handlers
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+# Register request and response interceptors
+sb.add_global_request_interceptor(LocalizationInterceptor())
+sb.add_global_request_interceptor(RequestLogger())
+sb.add_global_response_interceptor(ResponseLogger())
+
+# Handler name that is used on AWS lambda
+lambda_handler = sb.lambda_handler()
---
+++ lambda/prompts.py
@@ -0,0 +1,30 @@
+# Alexa Prompts Language Constants
+
+SKILL_NAME = "SKILL_NAME"
+GET_FACT_MESSAGE = "GET_FACT_MESSAGE"
+HELP_MESSAGE = "HELP_MESSAGE"
+HELP_REPROMPT = "HELP_REPROMPT"
+FALLBACK_MESSAGE = "FALLBACK_MESSAGE"
+FALLBACK_REPROMPT = "FALLBACK_REPROMPT"
+ERROR_MESSAGE = "ERROR_MESSAGE"
+STOP_MESSAGE = "STOP_MESSAGE"
+
+
+ANIMALS = [
+    'TRAINED PIGEONS CAN TELL THE DIFFERENCE BETWEEN THE PAINTINGS OF PABLO PICASSO AND CLAUDE MONET.',
+    'CAPUCHIN MONKEYS PEE ON THEIR HANDS TO WASH THEIR FEET.',
+    'BABY ELEPHANTS SUCK THEIR TRUNKS FOR COMFORT.',
+    'FLAMINGOS ARE NATURALLY WHITE—THEIR DIET OF BRINE SHRIMP AND ALGAE TURNS THEM PINK.',
+    'IN THE UK, THE BRITISH MONARCH LEGALLY OWNS ALL UNMARKED MUTE SWANS IN OPEN WATER.'
+]
+#https://www.mentalfloss.com/article/86578/50-incredible-animal-facts-youll-want-share
+
+PLANT = {
+    "Leaving the skin on potatoes while cooking is healthier as all the vitamins are in the skin.",
+    "Banana is an Arabic word for fingers.",
+    "Carrots were originally purple in colour.",
+    "The baobab tree found in Africa can store 1,000 to 120,000 litres of water in its swollen trunk.The baobab tree found in Africa can store 1,000 to 120,000 litres of water in its swollen trunk.",
+    "Bamboo is the fastest-growing woody plant in the world; it can grow 35 inches in a single day.",
+}
+
+#https://www.funfactsabout.net/plant-facts/
